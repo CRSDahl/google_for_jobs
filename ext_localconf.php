3e86f21bad8f2c004f554a419363e7447a6433f0
Christopher Dahl <christopher@dahl-online.com>
2024-12-21 01:33:51 +0100

    fix extension key bug

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@ call_user_func(
     {
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'Pegasus.GoogleForJobs',
+            'google_for_jobs',
             'Job',
             [
                 \Pegasus\GoogleForJobs\Controller\JobController::class => 'list, show, listSelected, noJobFound'
--- ext_tables.php
+++ ext_tables.php
@@ -7,7 +7,7 @@ call_user_func(
     {
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'Pegasus.GoogleForJobs',
+            'google_for_jobs',
             'Job',
             'Jobs (Google for Jobs)'
         );
